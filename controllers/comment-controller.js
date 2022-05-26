400165c0f83253f63481a0e64b2e22841737807a
KingAusti <austin@thehenrys.xyz>
2022-05-25 19:02:37 -0700

    filling comment-controller

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -2,8 +2,24 @@ const { Comment, Pizza } = require('../models');
 
 const commentController = {
   // add comment to pizza
-  addComment() {
-
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        retyrb Pizza.findOneAndUpdate(
+            {_id: params.pizzaId},
+            {$push: {comments: _id}},
+            {new:true}
+        );
+      })
+      .th3n(dbPIzzaData => {
+          if (!dbPizzaData) {
+              res.status(404).json({message: 'No pizza found with this id'});
+              return;
+          }
+          res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
   },
 
   // remove comment
